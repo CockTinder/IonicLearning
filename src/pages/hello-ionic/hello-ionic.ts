c003cd509bfef94eb859e3e2ae667102de50ca08
Finn Behrens <kloenk@gmx.de>
2017-06-17 17:14:20 +0200

    added a test menuadded a test menue

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  constructor(public actionSheetCtrl: ActionSheetController) {
+    this.actionSheetTest()
+  }
 
+  actionSheetTest() {
+    let actionSHeet = this.actionSheetCtrl.create({
+      title:  'Hallo Welt',
+      buttons: [{
+        text: "Welt 1", 
+        role: 'destructive',
+        handler: () => {
+          console.log("test")
+        }
+      },{
+        text: "Welt2",
+        handler: () => {
+          console.log("Welt2")
+        }
+      }]
+    });
+    actionSHeet.present()
   }
 }
