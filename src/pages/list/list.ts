47018642d9252a27af60cf496b7c7025cf936c5c
Finn Behrens <kloenk@gmx.de>
2017-06-17 15:05:24 +0200

    added a alert test

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -4,6 +4,8 @@ import { NavController, NavParams } from 'ionic-angular';
 
 import { ItemDetailsPage } from '../item-details/item-details';
 
+import { AlertController } from 'ionic-angular';
+
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
@@ -12,7 +14,7 @@ export class ListPage {
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
     'american-football', 'boat', 'bluetooth', 'build'];
 
@@ -27,8 +29,39 @@ export class ListPage {
   }
 
   itemTapped(event, item) {
+    this.showPrompt()
     this.navCtrl.push(ItemDetailsPage, {
       item: item
     });
   }
+
+  showPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'Login',
+      message: "Enter a name for this new album you're so keen on adding",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+    console.log("test")
+    prompt.present();
+  }
 }
+
